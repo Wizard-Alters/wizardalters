06547b64709f932f28af0bb0b9ded8ebf3f87907
William Torman <willtorman@gmail.com>
2025-09-07 21:40:18 -0600

    start contact modal

--- src/components/contact/ContactModal.jsx
+++ src/components/contact/ContactModal.jsx
@@ -1,22 +1,22 @@
 import { useState } from "react";
 
-export default function ContactModal () {
-    // const [openModal, setOpenModal] = useState;
+export default function ContactModal() {
+  // const [openModal, setOpenModal] = useState;
 
-    return (
-      <>
-        <form>
-          <label for="fname">First Name</label>
-          <input
-            type="text"
-            id="fname"
-            name="firstname"
-            placeholder="Your name..."
-          ></input>
-          <label for="lname">Last Name</label>
-          <input>
-          </input>
-        </form>
-      </>
-    );
+  return (
+    <>
+      <form>
+        <label for="fname">First Name</label>
+        <input
+          type="text"
+          id="fname"
+          name="firstname"
+          placeholder="Your name..."
+        ></input>
+        <label for="lname">Last Name</label>
+        <input 
+        type="text" id="lname" placeholder="Your last name..."></input>
+      </form>
+    </>
+  );
 }
